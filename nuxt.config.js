dd29c6475e3ec37191abf0ced15a96b3a2459efa
galaxychoi <galaxyceh@gmail.com>
2021-07-12 16:08:12 +0900

    add Bulma css

---
+++ nuxt.config.js
@@ -0,0 +1,65 @@
+export default {
+  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+  ssr: false,
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: '강냥이의 동행ㅇ',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: '강아지와 고양이의 동행' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: "stylesheet",
+        href: "https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css"
+      }
+    ],
+    script: [{
+      defer: "",
+      src: "https://use.fontawesome.com/releases/v5.15.3/js/all.js"
+    }]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module'
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+    // https://go.nuxtjs.dev/pwa
+    '@nuxtjs/pwa'
+  ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {},
+
+  // PWA module configuration: https://go.nuxtjs.dev/pwa
+  pwa: {
+    manifest: {
+      lang: 'en'
+    }
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <section class="hero is-primary">
+      <div class="hero-body">
+        <p class="title">강아지와 고양이</p>
+        <p class="subtitle">강아지와 고양이에 대한 모든 정보를 볼수 있는 곳</p>
+      </div>
+    </section>
+  </div>
+</template>
+
