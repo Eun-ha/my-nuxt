6da71be1dd19d1fec72529857e2e97f6972726a5
galaxychoi <galaxyceh@gmail.com>
2021-07-12 17:52:02 +0900

    배포 test

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,9 @@
   <div>
     <section class="hero is-primary">
       <div class="hero-body">
-        <p class="title">강아지와 고양이</p>
+        <p class="title">
+          <i class="fas fa-dog"></i>강아와 <i class="fas fa-cat"></i>고양
+        </p>
         <p class="subtitle">강아지와 고양이에 대한 모든 정보를 볼수 있는 곳</p>
       </div>
     </section>
