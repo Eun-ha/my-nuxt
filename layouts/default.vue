113babcfc95e235b8c8c02f92f44adb5cdd0d45d
galaxychoi <galaxyceh@gmail.com>
2021-08-05 17:12:43 +0900

    nav footer 추가

---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <nav class="breadcrumb">
+      <ul>
+        <li>
+          <a href="#"><i class="fas fa-home"></i>Bulma</a>
+        </li>
+        <li><a href="#">Documentation</a></li>
+        <li><a href="#">Components</a></li>
+        <li class="is-active">
+          <a href="#" aria-current="page">Breadcrumb</a>
+        </li>
+      </ul>
+    </nav>
+    <nuxt />
+    <footer class="footer">
+      <div class="content has-text-centered">
+        <i class="fas fa-copyright"></i> 2021-08-05 restart
+      </div>
+    </footer>
+  </div>
+</template>
