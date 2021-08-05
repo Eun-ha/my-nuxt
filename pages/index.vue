0219ee2f0b54694914c6623d0601f824acae8d53
galaxychoi <galaxyceh@gmail.com>
2021-08-05 16:57:00 +0900

    이미지 내용 추가

--- pages/index.vue
+++ pages/index.vue
@@ -8,6 +8,72 @@
         <p class="subtitle">강아지와 고양이에 대한 모든 정보를 볼수 있는 곳</p>
       </div>
     </section>
+    <div class="columns">
+      <div class="column">
+        <article class="message">
+          <div class="message-header"><p>강아지 정보</p></div>
+          <div class="message-body">
+            <figure class="image is-3by5">
+              <img
+                src="https://www.cdc.gov/healthypets/images/pets/cute-dog-headshot.jpg"
+                alt=""
+              />
+            </figure>
+            The domestic dog (Canis familiaris or Canis lupus familiaris)[4] is
+            a domesticated descendant of the wolf. The dog derived from an
+            ancient, extinct wolf,[5][6] and the modern grey wolf is the dog's
+            nearest living relative.[7] The dog was the first species to be
+            domesticated,[8][7] by hunter–gatherers over 15,000 years ago,[6]
+            before the development of agriculture.[1] Their long association
+            with humans has led dogs to be uniquely adapted to human
+            behavior,[9] leading to a large number of domestic individuals[10]
+            and the ability to thrive on a starch-rich diet that would be
+            inadequate for other canids.[11] The dog has been selectively bred
+            over millennia for various behaviors, sensory capabilities, and
+            physical attributes.[12] Dog breeds vary widely in shape, size, and
+            color. They perform many roles for humans, such as hunting, herding,
+            pulling loads, protection, assisting police and the military,
+            companionship, therapy, and aiding disabled people. This influence
+            on human society has given them the sobriquet of "man's best
+            friend."
+          </div>
+        </article>
+      </div>
+      <div class="column">
+        <article class="message">
+          <div class="message-header"><p>고양이 정보</p></div>
+          <div class="message-body">
+            <div>
+              <figure class="image is-3by5">
+                <img src="~assets/cat.jpg" alt="" />
+              </figure>
+              <figure class="image is-3by5">
+                <img src="~static/cat.jpg" alt="" />
+              </figure>
+            </div>
+            <p>
+              The domestic dog (Canis familiaris or Canis lupus familiaris)[4]
+              is a domesticated descendant of the wolf. The dog derived from an
+              ancient, extinct wolf,[5][6] and the modern grey wolf is the dog's
+              nearest living relative.[7] The dog was the first species to be
+              domesticated,[8][7] by hunter–gatherers over 15,000 years ago,[6]
+              before the development of agriculture.[1] Their long association
+              with humans has led dogs to be uniquely adapted to human
+              behavior,[9] leading to a large number of domestic individuals[10]
+              and the ability to thrive on a starch-rich diet that would be
+              inadequate for other canids.[11] The dog has been selectively bred
+              over millennia for various behaviors, sensory capabilities, and
+              physical attributes.[12] Dog breeds vary widely in shape, size,
+              and color. They perform many roles for humans, such as hunting,
+              herding, pulling loads, protection, assisting police and the
+              military, companionship, therapy, and aiding disabled people. This
+              influence on human society has given them the sobriquet of "man's
+              best friend."
+            </p>
+          </div>
+        </article>
+      </div>
+    </div>
   </div>
 </template>
 
